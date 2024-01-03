a334577567d621907bd93ef49fdab76288fd41da
Medvidek77 <121556205+Medvidek77@users.noreply.github.com>
2024-01-03 19:24:02 +0100

    1

---
+++ test.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+import asyncio
+
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user.name} is ready")
+
+@bot.slash_command(name="test", description="test command")
+async def test(ctx):
+    await ctx.send("test")
+
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    if message.content == "test":
+        await message.channel.send("test")
+
+    
+
+
+
+bot.run("MTE4MjczMzE1MTAxODE2MDI4OA.Gl61RG.cIARIedw7Tpgql8NdxKZDqOohY1PXFTRlL4opo")
